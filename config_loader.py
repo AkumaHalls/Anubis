21ac9e1f50f8e9f4148a752832958827393d67d2
AkumaHalls <153091442+AkumaHalls@users.noreply.github.com>
2024-04-12 23:13:54 -0300

    Update config_loader.py

--- config_loader.py
+++ config_loader.py
@@ -43,7 +43,7 @@ DEFAULT_CONFIG = {
     #########################
     ### Sistema de música ###
     #########################
-    "AUTO_DOWNLOAD_LAVALINK_SERVERLIST": False,
+    "AUTO_DOWNLOAD_LAVALINK_SERVERLIST": True,
     "LAVALINK_SERVER_LIST": "https://gist.githubusercontent.com/zRitsu/c3511e1da0440b94c126769dd40c9d91/raw/lavalink.ini",
     "LAVALINK_RECONNECT_RETRIES": 30,
     "DEFAULT_SKIN": "default",
